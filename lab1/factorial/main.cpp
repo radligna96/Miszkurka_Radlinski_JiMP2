d7b1b3f3f87e0b394bf617b13f36549b262dfab0
Radliński Ignacy <radlinsk@student.agh.edu.pl>
2017-03-14 19:16:00 +0100

    fmlfmlfml

--- lab1/factorial/main.cpp
+++ lab1/factorial/main.cpp
@@ -2,6 +2,6 @@
 #include "Factorial.h"
 
 int main() {
-  std::cout << "factorial of 20 is: " << factorial(20) << std::endl;
+  std::cout <<"FML "<< "factorial of 20 is: " << factorial(20) << std::endl;
   return 0;
 }
