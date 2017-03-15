fa65a0e6c5ebf4cf1dea5be3588c25cfd24279a1
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2017-03-15 11:25:18 +0100

    commit agi

--- lab1/factorial/main.cpp
+++ lab1/factorial/main.cpp
@@ -2,6 +2,6 @@
 #include "Factorial.h"
 
 int main() {
-  std::cout <<"FML "<< "factorial of 20 is: " << factorial(20) << std::endl;
+  std::cout <<" nanana "<< "factorial of 20 is: " << factorial(20) << std::endl;
   return 0;
 }
