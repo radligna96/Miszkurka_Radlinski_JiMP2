7539589c32f0b0d4af5a99757dce6f441e467cfe
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2017-03-27 19:24:48 +0200

    lab4 dOstateczna wersja

--- tests/lab4test/SimpleTemplateEngineTest.cpp
+++ tests/lab4test/SimpleTemplateEngineTest.cpp
@@ -7,8 +7,7 @@
 #include <memory>
 #include <MemLeakTest.h>
 #include <StringUtility.h>
-//#include <SimpleTemplateEngine.h>
-#include "../../lab4/simpletemplateengine/SimpleTemplateEngine.h"
+#include <SimpleTemplateEngine.h>
 
 using ::nets::View;
 using ::std::make_unique;
