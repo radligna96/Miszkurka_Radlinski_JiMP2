8cdc32248ae35520bcce7bf4523c1db3e150edf9
radligna96 <radlinsk@student.agh.edu.pl>
2017-03-28 10:48:40 +0200

    lab4 complete lab5 start

--- lab5/xxx/Xxx.h
+++ lab5/xxx/Xxx.h
@@ -5,4 +5,44 @@
 #ifndef JIMP_EXERCISES_XXX_H
 #define JIMP_EXERCISES_XXX_H
 
+using namespace std;
+
+#include <iostream>
+#include <cstring>
+#include <string>
+
+class XXX {
+    //w zeszłym odcinku:
+    //domyślny konstruktor
+    XXX();
+    //konstruktory parametryczne
+    XXX(int param);
+    XXX(const std::string name);
+
+    //Rule of five://
+    //1. konstruktor kopiujący
+    XXX(const XXX &xxx);
+    //2. konstruktor przenoszący
+    XXX(XXX &&xxx);
+    //3. operator przypisania kopiujący
+    XXX &operator=(const XXX &xxx);
+    //4. operator przypisania przenoszący
+    XXX &operator=(XXX &&xxx);
+
+    XXX() : name_{new char[1024]} {
+
+    }
+
+
+private:
+    char *name_;
+    //...
+    //5. destruktor:
+    ~XXX() {
+        delete [] name_;
+
+}
+
+
+
 #endif //JIMP_EXERCISES_XXX_H
