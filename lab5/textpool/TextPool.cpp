075ed84c1f00fd7c1fcb7ec7409d157036082748
radligna <radlinsk@student.agh.edu.pl>
2017-04-06 03:41:18 +0200

    lab 5 COMPLETE!

--- lab5/textpool/TextPool.cpp
+++ lab5/textpool/TextPool.cpp
@@ -7,53 +7,46 @@
 using pool::TextPool;
 
 
-    //konstruktor przenoszący:
-    TextPool::TextPool(TextPool &&text) {
-        std::swap(perla,text.perla);
-        //Bardzo popularna szutczka
-        //wiemy, ze za chwilę xxx zostanie zniszczony
-        //za pomocą destrukotra, więc inicjalizujemy
-        //this na nullptr i wymieniamy się z xxx
-        //delete nullptr jest bezpieczna operacją i nic się nie stanie...
+TextPool::TextPool(){};
+
+TextPool::TextPool(TextPool &&text) {
+    swap(perla,text.perla);
+}
+
+TextPool & TextPool::operator=(TextPool &&text)  {
+    if (this == &text) {
+        return text;
     }
+    perla.clear();
+    swap(perla,text.perla);
+    return *this;
+}
 
-    //operator przenoszący:
-    TextPool & TextPool::operator=(TextPool &&text)  {
-        //jeśli ktoś wpadł na pomsył x = move(x);
-        if (this == &text) {
-            return text;
-        }
-        perla.clear();
-        std::swap(perla,text.perla);
-        return *this;
-    }
+TextPool::~TextPool() {
+    perla.clear();
+};
 
-    TextPool::~TextPool()
-    {
-        perla.clear();
-    };
+TextPool::TextPool(const std::initializer_list<string_view> &some_list) {
 
-    TextPool::TextPool(){};
+    for (auto i : some_list)
+        perla.emplace(i);
+}
 
-    TextPool::TextPool(const std::initializer_list<const string> &some_list) {
+size_t TextPool::StoredStringCount() const {
 
-        for(auto element : some_list)
-        {
-            perla.emplace(element);
-        }
-    }
+    unsigned long size = perla.size();
+    return size;
+}
 
+std::experimental::string_view TextPool::Intern(const std::string &str) {
 
-    size_t TextPool::StoredStringCount() const {
-
-        unsigned long size = perla.size();
-        return size;
-    }
-
-    std::experimental::string_view TextPool::Intern(const std::string &str) {
-
-        if(perla.find(str)==perla.end())
-        {
-            perla.emplace(str);
+    for (auto i : perla) {
+        if (i == str){
+            return i;
         }
     }
+
+    perla.emplace(str);
+    return Intern(str);
+
+}
--- lab5/textpool/TextPool.h
+++ lab5/textpool/TextPool.h
@@ -16,36 +16,35 @@ using std::experimental::string_view;
 
 namespace pool {
     class TextPool {
-    protected:
-        std::set<const string_view> perla;
+    private:
+        std::set<string_view> perla;
 
     public:
-        //Rule of Five (z usuniętą możliwością kopiowania)
-
-        //1. konstruktor kopiujący
-        //TextPool(const TextPool &xxx);
-
         //2. konstruktor przenoszący
         TextPool(TextPool &&xxx);
 
-        //3. operator przypisania kopiujący
-        //TextPool &operator=(const TextPool &xxx);
-
         //4. operator przypisania przenoszący
-        TextPool &operator=(TextPool &&xxx);
+        TextPool & operator=(TextPool &&xxx);
 
         //5. Destruktor
         ~TextPool();
-
 
         //domyślny konstruktor
         TextPool();
 
         //konstruktor z listą inicjalizacyjną
-        TextPool(const std::initializer_list<const std::string> &elements);
+        TextPool(const std::initializer_list<string_view> &elements);
 
         std::experimental::string_view Intern(const std::string &str);
         size_t StoredStringCount() const;
+
+    protected:
+
     };
 }
 #endif //JIMP_EXERCISES_TEXTPOOL_H
+/*
+
+
+
+ */
