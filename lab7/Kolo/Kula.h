9b46188b63121416c341c21f3465b40ffd0f4d44
miszagni <miszkur@student.agh.edu.pl>
2017-04-11 10:53:59 +0200

    lab6:student

---
+++ lab7/Kolo/Kula.h
@@ -0,0 +1,26 @@
+//
+// Created by miszagni on 11.04.17.
+//
+
+#ifndef JIMP_EXERCISES_KULA_H
+#define JIMP_EXERCISES_KULA_H
+
+#include "Kolo.h"
+#include <iostream>
+using namespace std;
+
+
+class Kula : Kolo
+{
+    double z;
+    //double pole();
+public:
+    Kula(){};
+    Kula(double x_, double y_, double z_, double r_):Kolo(x_, y_, r_)
+    {
+        z = z_;
+    };
+};
+
+
+#endif //JIMP_EXERCISES_KULA_H
