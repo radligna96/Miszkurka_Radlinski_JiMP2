cb3650dd4ff16507d0f091fc2d34ee782af42d8c
radligna96 <radlinsk@student.agh.edu.pl>
2017-04-11 10:54:00 +0200

    lab7 ignac

---
+++ lab7/arrayfill/ArrayFill.h
@@ -0,0 +1,36 @@
+//
+// Created by radligna on 11.04.17.
+//
+
+#ifndef JIMP_EXERCISES_ARRAYFILL_H
+#define JIMP_EXERCISES_ARRAYFILL_H
+
+#include <vector>
+using std::vector;
+using ::arrays::IncrementalFill;
+using ::arrays::FillArray;
+
+namespace arrays {
+
+    class FillArray {
+
+    private:
+        vector<int> arr;
+
+    public:
+        void fill(FillArray);
+
+
+    };
+
+    class IncrementalFill : FillArray{
+
+    public:
+
+
+
+    };
+
+}
+
+#endif //JIMP_EXERCISES_ARRAYFILL_H
---
+++ lab7/arrayfill/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by radligna on 11.04.17.
+//
+
