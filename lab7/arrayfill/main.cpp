48c577dc788bb6f6b938cd71fe74f73dfd98a742
radligna <radlinsk@ttudent.agh.edu.pl>
2017-04-16 22:48:26 +0200

    ArrayFill-e dzialaja !!! o-ho-hooo oo o hooo

--- lab7/arrayfill/main.cpp
+++ lab7/arrayfill/main.cpp
@@ -2,3 +2,22 @@
 // Created by radligna on 11.04.17.
 //
 
+#include "ArrayFill.h"
+
+using ::arrays::RandomFill;
+using ::arrays::ArrayFill;
+using ::arrays::FillArray;
+
+int main() {
+
+    auto expected_generator = default_random_engine {};
+    auto expected_distribution = uniform_int_distribution<int> {1,6};
+
+    for (int i = 0; i < 10; i++)
+        std::cout<<expected_distribution(expected_generator);
+
+
+
+
+    return 0;
+}
