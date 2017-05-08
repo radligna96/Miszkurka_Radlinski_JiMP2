3904f97498a6638a5b61790ae6f954faf1a3fb9f
agnieszka-miszkurka <miszkur@student.agh.edu.pl>
2017-05-08 18:33:40 +0200

    lab8 movie subtitles compete

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -6,7 +6,6 @@
 
 void moviesubs::SubRipSubtitles::ShiftAllSubtitlesBy(int delay, int fps, stringstream *in, stringstream *out) {
 
-
     regex pattern {R"((\d{2}):(\d{2}):(\d{2}),(\d{3}) --> (\d{2}):(\d{2}):(\d{2}),(\d{3}))"};
     regex pattern_start {R"((^\d\n))"};
     regex pattern_frame_order {R"((\n\d\n))"};
@@ -15,54 +14,49 @@ void moviesubs::SubRipSubtitles::ShiftAllSubtitlesBy(int delay, int fps, strings
     smatch matches_first_frame;
     smatch frame_order;
     smatch frame;
-    bool maria = false;
+    bool flag = false;
     int line_number=0;
+
     if(fps < 0 )
-        throw std::invalid_argument("fps ujemny lol");
+        throw std::invalid_argument("error 404: invalid argument");
 
-    string in_string = "";
-    string moze_nie = (*in).str();
-
+    string in_string_cp = (*in).str();
     //ustawiamy prior na pierwsza klatke
-    std::regex_search(moze_nie, matches_first_frame, pattern_start);
+    std::regex_search(in_string_cp, matches_first_frame, pattern_start);
         int first_frame_number = stoi(matches_first_frame[1]);
-
     int prior = first_frame_number;
 
     //sprawdzamy reszta klatek czy jest w kolejnosci
-    std::regex_search(moze_nie, frame_order, pattern_frame_order);
+    std::regex_search(in_string_cp, frame_order, pattern_frame_order);
     for(int i = 1; i < frame_order.size() ; i++, prior++)
     {
         if (stoi(frame_order[i]) != prior+1)
             throw OutOfOrderFrames();
     }
 
+    string in_string = "";
     while(getline(*in,in_string,'\n')) {
 
         if (std::regex_search(in_string, matches, pattern)) {
             line_number++;
-            maria = false;
+            flag = false;
 
             *out << matches[1] << ":" << matches[2] << ":";
+
+            int match_4_delayed = stoi(matches[4].str()) + delay;
+            int match_3_delayed = stoi(matches[3])+match_4_delayed/1000;
+            string match_3_str = to_string(match_3_delayed);
             string match_4_str = matches[4].str();
-            int match_4_dealyed = stoi(matches[4].str()) + delay;
-            int match_3_str_int = stoi(matches[3])+match_4_dealyed/1000;
-            string match_3_str = to_string(match_3_str_int);
 
-
-            if(match_4_dealyed<1000)
-            {
-                while(match_4_str.size()<3)
+            if(match_4_delayed < 1000) {
+                while(match_4_str.size() < 3)
                     match_4_str='0'+match_4_str;
                 while(match_3_str.size()<2)
                     match_3_str='0'+match_3_str;
-                (*out)<< match_3_str << "," << match_4_dealyed << " --> ";
+                (*out)<< match_3_str << "," << match_4_delayed << " --> ";
             }
-            else{
-                match_3_str_int = stoi(matches[3])+match_4_dealyed/1000;
-                match_4_dealyed %= 1000;
-                match_4_str = to_string(match_4_dealyed);
-                match_3_str = to_string(match_3_str_int);
+            else {
+                match_4_str = to_string(match_4_delayed%1000);
 
                 while(match_4_str.size()<3)
                     match_4_str='0'+match_4_str;
@@ -73,11 +67,11 @@ void moviesubs::SubRipSubtitles::ShiftAllSubtitlesBy(int delay, int fps, strings
 
             *out << matches[5] << ":" << matches[6] << ":";
             string match_8_str = matches[8].str();
-            int match_8_str_int = stoi(matches[8].str()) + delay;
-            int match_7_str_int = stoi(matches[7])+match_8_str_int/1000;
-            string match_7_str = to_string(match_7_str_int);
+            int match_8_delayed = stoi(matches[8].str()) + delay;
+            int match_7_delayed = stoi(matches[7])+match_8_delayed/1000;
+            string match_7_str = to_string(match_7_delayed);
 
-            if(match_8_str_int<1000)
+            if(match_8_delayed < 1000)
             {
                 while(match_8_str.size() < 3)
                     match_8_str = '0' + match_8_str;
@@ -87,33 +81,28 @@ void moviesubs::SubRipSubtitles::ShiftAllSubtitlesBy(int delay, int fps, strings
                 (*out)<< match_7_str << "," << match_8_str << " --> ";
             }
             else{
-                match_7_str_int = stoi(matches[7])+match_8_str_int/1000;
-                match_8_str_int %= 1000;
-                match_8_str = to_string(match_8_str_int);
-                match_7_str = to_string(match_7_str_int);
+                match_8_str = to_string(match_8_delayed%1000);
 
                 while(match_8_str.size() < 3)
                     match_8_str = '0' + match_8_str;
-                while(match_7_str.size() < 2)   //XDD przypau radlinski  match_3_str.size()
+                while(match_7_str.size() < 2)
                     match_7_str = '0' + match_7_str;
                 (*out)<< match_7_str << "," << match_8_str << '\n';
             }
-            if(match_3_str_int < 0 || match_4_dealyed < 0 || match_7_str_int < 0 || match_8_str_int < 0) {
-                {
-                    throw NegativeFrameAfterShift();
-                }
-            }
+
+            if(match_3_delayed < 0 || match_4_delayed < 0 || match_7_delayed < 0 || match_8_delayed < 0)
+                throw NegativeFrameAfterShift();
+
             if (stoi(matches[1]) * 3600 + stoi(matches[2]) * 60 + stoi(matches[3]) + stoi(matches[4]) * 0.001 >= stoi(matches[5]) * 3600 + stoi(matches[6]) * 60 + stoi(matches[7]) + stoi(matches[8]) * 0.001)
-            {
                 throw moviesubs::SubtitleEndBeforeStart(line_number, in_string);
-            }
+
         }
         else {
-            if(maria)
+            if(flag)
                 throw InvalidSubtitleLineFormat();
 
             if (std::regex_search(in_string, frame, pattern_invalid_format))
-                maria = true;
+                flag = true;
 
             *out << in_string << '\n';
             }
